423774871640700461983bd74c81f93a55c2ab4f
noelhorvath <hnoel2000@gmail.com>
2022-03-21 15:05:27 +0100

    add AppSettings.ts for project specific configurations

---
+++ src/app/app.settings.ts
@@ -0,0 +1,7 @@
+export const AppSettings = {
+    name: 'Mi Band 3 Companion',
+    packageName: 'hu.noelhorvath.MiBand3Companion',
+    codeName: 'MiBand3Companion',
+    version: '0.5.0',
+    enableFirebaseDataCollection: false,
+};
