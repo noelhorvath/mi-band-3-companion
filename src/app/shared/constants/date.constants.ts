db60b2b91e063c059df029cc13b3c6bff4ec480c
noelhorvath <hnoel2000@gmail.com>
2022-03-21 15:13:58 +0100

    add date constants

---
+++ src/app/shared/constants/date.constants.ts
@@ -0,0 +1,26 @@
+import { Days, Months } from '../enums/date.enum';
+
+export const MONTHS: string[] = [
+    Months.JANUARY,
+    Months.FEBRUARY,
+    Months.MARCH,
+    Months.APRIL,
+    Months.MAY,
+    Months.JUNE,
+    Months.JULY,
+    Months.AUGUST,
+    Months.SEPTEMBER,
+    Months.OCTOBER,
+    Months.NOVEMBER,
+    Months.DECEMBER
+];
+
+export const DAYS: string[] = [
+    Days.MONDAY,
+    Days.TUESDAY,
+    Days.WEDNESDAY,
+    Days.THURSDAY,
+    Days.FRIDAY,
+    Days.SATURDAY,
+    Days.SUNDAY,
+];
