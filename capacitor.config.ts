f1dca1e77545fae9e1a862b4de48c4d4ac0a8621
noelhorvath <hnoel2000@gmail.com>
2022-03-21 14:56:46 +0100

    configure Capacitor settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,35 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli';
+import { environment } from './src/environments/environment';
 
 const config: CapacitorConfig = {
-  appId: 'hu.noelhorvath.miband3companion',
-  appName: 'Mi Band 3 Companion',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  loggingBehavior: 'debug',
-  plugins: { }
-}
+    appId: 'hu.noelhorvath.miband3companion',
+    appName: 'Mi Band 3 Companion',
+    webDir: 'www',
+    bundledWebRuntime: false,
+    loggingBehavior: environment.production ? 'production' : 'debug',
+    server: {
+        hostname: 'localhost'
+    },
+    plugins: {
+        /*
+        // TODO: configure splash screen
+        SplashScreen: {
+            launchShowDuration: 3000,
+            launchAutoHide: true,
+            backgroundColor: '#ff9500',
+            //androidSplashResourceName: 'splash',
+            androidScaleType: 'FIT_CENTER',
+            showSpinner: true,
+            androidSpinnerStyle: 'small',
+            iosSpinnerStyle: 'small',
+            spinnerColor: '#ffffff',
+            splashFullScreen: true,
+            splashImmersive: true,
+            layoutName: 'launch_screen',
+            useDialog: false,
+        },
+        */
+    },
+};
 
 export default config;
