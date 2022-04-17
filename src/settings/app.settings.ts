c1e4b741d6f066805177d89d5cd0c56be04d75c8
noelhorvath <hnoel2000@gmail.com>
2022-04-17 20:38:50 +0200

    update version in settings

--- src/settings/app.settings.ts
+++ src/settings/app.settings.ts
@@ -2,7 +2,7 @@ export const AppSettings = {
     name: 'Mi Band 3 Companion',
     packageName: 'hu.noelhorvath.MiBand3Companion',
     codeName: 'MiBand3Companion',
-    version: '0.5.0',
+    version: '0.9.9',
     enableFirebaseDataCollection: false,
     //appDynamicLinksUrl: 'https://www.mibandcompanion.page.link'
 };
