27cf5074f58d2d4e17a1fb369c91387c92b4aefc
noelhorvath <hnoel2000@gmail.com>
2022-04-17 20:37:47 +0200

    update firestore rules

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,30 @@
-// import * as functions from "firebase-functions";
+/*
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { IActivity } from '../../../src/app/shared/models/interfaces/IActivity';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const onCreateActivity = functions.firestore
+    .document('userData/{userId}/activity/{deviceId}')
+    .onCreate( async (snap: functions.firestore.QueryDocumentSnapshot, context: functions.EventContext): Promise<void> => {
+        try {
+            const collectionRef = firestore.collection(`userData/${ context.params.userId }/activityData`);
+            const querySnap = await collectionRef.where('measurementInfo.date', '==', snap.data().measurementInfo.date).limit(1).get();
+            if (!querySnap.empty && querySnap.docs[0].exists) {
+                const activity = snap.data() as IActivity;
+                // existing activity data with synced activity
+                await querySnap.docs[0].ref.update({
+                    steps: activity.steps,
+                    category: activity.category,
+                    intensity: activity.intensity
+                });
+                // delete created synced activity
+                await snap.ref.delete({ exists: true });
+            }
+        } catch (e: unknown) {
+            console.error(onCreateActivity.name + ' error: ' + JSON.stringify(e));
+        }
+    });
+*/
